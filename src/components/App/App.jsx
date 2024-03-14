eaa8e66bfdfeccaa264e861e0094c4102c72f325
Aileneboro <a.borovikova89@gmail.com>
2024-03-15 00:31:15 +0200

    markup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import userData from "../../userData.json";
 import Profile from "../Profile/Profile";
+import friends from "../../friends.json";
+import FriendList from "../FriendList/FriendList";
+import transactions from "../../transactions.json";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
 
 const App = () => {
   return (
@@ -12,6 +16,8 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
