8b529a1c7930bcf7db81945d61707bb3939f2917
Aileneboro <a.borovikova89@gmail.com>
2024-03-15 15:13:33 +0200

    task1 css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,22 +1,5 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
-
-// export default function FriendList({ friends }) {
-//   return (
-//     <ul>
-//       {friends.map((friend) => {
-//         return (
-//           <li key={friend.id}>
-//             <FriendListItem
-//               avatar={friend.avatar}
-//               name={friend.name}
-//               isOnline={friend.isOnline}
-//             />
-//           </li>
-//         );
-//       })}
-//     </ul>
-//   );
-// }
+import css from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,26 @@
+import css from "./Profile.module.css";
+
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
+    <div className={css["container-profile"]}>
       <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <img src={image} alt="User avatar" className={css["profile-avatar"]} />
+        <p className={css["profile-name"]}>{name}</p>
+        <p className={css["profile-tag"]}>@{tag}</p>
+        <p className={css["profile-location"]}>{location}</p>
       </div>
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css["profile-stats"]}>
+        <li className={css["profile-stats-items"]}>
+          <span className={css["stats-title"]}>Followers</span>
+          <span className={css["stats-amount"]}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css["profile-stats-items"]}>
+          <span className={css["stats-title"]}>Views</span>
+          <span className={css["stats-amount"]}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css["profile-stats-items"]}>
+          <span className={css["stats-title"]}>Likes</span>
+          <span className={css["stats-amount"]}>{stats.likes}</span>
         </li>
       </ul>
     </div>
