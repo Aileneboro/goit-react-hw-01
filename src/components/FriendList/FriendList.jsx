6b59cc257bd444be569c2cca75f0c5a2f073d152
Aileneboro <a.borovikova89@gmail.com>
2024-03-15 10:59:19 +0200

    upd

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,24 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
 
-export default function FriendList({ friends }) {
+// export default function FriendList({ friends }) {
+//   return (
+//     <ul>
+//       {friends.map((friend) => {
+//         return (
+//           <li key={friend.id}>
+//             <FriendListItem
+//               avatar={friend.avatar}
+//               name={friend.name}
+//               isOnline={friend.isOnline}
+//             />
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// }
+
+const FriendList = ({ friends }) => {
   return (
     <ul>
       {friends.map((friend) => {
@@ -16,4 +34,6 @@ export default function FriendList({ friends }) {
       })}
     </ul>
   );
-}
+};
+
+export default FriendList;
